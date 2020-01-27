b063fc08af54bde45ffdf1f70c2d46eb24dd05bd
prasiska <prasiskajuliyanti13@gmail.com>
2020-01-27 16:00:14 +0700

    belajar faker

---
+++ database/migrations/2020_01_27_073622_create__customer_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Customer', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('code_customer')->unique();
+            $table->string('name');
+            $table->string('email');
+            $table->string('country');
+            $table->string('city');
+            $table->text('address');
+            $table->string('contact_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Customer');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(siska::class);
         // $this->call(SiswaSeeder::class);
-        $this->call(PostsTablebio::class);
+        //$this->call(PostsTablebio::class);
+        factory(App\Tabungan::class, 100)->create();
+        factory(App\Customer::class, 1000)->create();
         //$this->call(UsersTableSeeder:class);
     }
 }
